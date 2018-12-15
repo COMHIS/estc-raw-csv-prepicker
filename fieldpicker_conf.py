f5c1fc81e6953edd87707b9dfb218d1fde54766e
antagomir <leo.lahti@iki.fi>
2018-12-15 18:47:05 +0200

    language

--- fieldpicker_conf.py
+++ fieldpicker_conf.py
@@ -13,10 +13,10 @@ estc_csv = "../estc-data-verified/estc-csv-raw-filtered/estc_raw_sane.csv"
 # Also, if subfield has the value 'all', all subfields will be kept.
 # If you want to have multiple subfields, but not all per one field, add
 # an entry for each combination.
-fields_keep = [{'field': '035', 'subfield': 'a'},
-               {'field': '300', 'subfield': 'c'}]
-
-fields_outfile = "out/fields_picked_300c.csv"
+fields_keep = [{'field': '008'}, 
+               {'field': '041'} 
+               ]
+fields_outfile = "out/fields_picked_language.csv"
 
 # field 035 a has the CU-RivES info. You'll prob want to keep that.
 # examples:
